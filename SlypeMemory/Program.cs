4c254bf54c44da1a06b305c10e29d271eab062d5
Slype <likeskippyshow@gmail.com>
2019-05-30 18:48:44 +0200

    Add project files.

---
+++ SlypeMemory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SlypeMemory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
